e6cacdb03a7578fc37b4dfd95fdd3e6746bc89ba
jvguima <joaovictorgmv@gmail.com>
2024-11-18 20:04:55 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/teste.html");
+
+});
+
+app.listen(port, () => {
+    console.log("Aplicação sendo executada na porta http://localhost:" + port);
+});
+
